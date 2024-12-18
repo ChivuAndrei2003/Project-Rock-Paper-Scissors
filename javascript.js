f32f55a08901917f083b3b7c4c239e0c39502ede
ChivuAndrei2003 <chivuandrei2003@gmail.com>
2024-12-18 18:47:40 +0000

    functie care returneaza rock,paper sau scissors random

---
+++ javascript.js
@@ -0,0 +1,26 @@
+function getComputerChoice() {
+    let optiune1 = "paper";
+    let optiune2 = "scissors";
+    let optiune3 = "rock";
+    let value = Math.random();
+    
+    if (value <= 1/3) {
+        value = optiune1;
+    } else if (value > 1/3 && value < 2/3) {
+        value = optiune2;
+    } else {
+        value = optiune3;
+    }
+
+    console.log(value); 
+}
+
+getComputerChoice(); 
+
+
+   
+
+
+
+
+*/
